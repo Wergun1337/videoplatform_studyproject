22452ca1f2526afe416411d112618b601f1fffcd
Wergun1337 <derpywhoo1337@gmail.com>
2026-05-27 23:21:38 +0300

    feat:minista.config updates

--- minista.config.js
+++ minista.config.js
@@ -76,7 +76,11 @@ export default defineConfig({
       localsConvention: 'camelCaseOnly',
     },
     preprocessorOptions: {
-      scss: {},
+      scss: {
+        additionalData:`
+        @use '@/styles/helpers' as *;
+        `
+      },
       less: {},
       stylus: {},
     },
