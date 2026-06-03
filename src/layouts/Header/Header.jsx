e540bfc4e08b2e8fb8e34508065a6f92704c793c
Wergun1337 <derpywhoo1337@gmail.com>
2026-06-04 00:03:42 +0300

    burger menu component added

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -32,7 +32,8 @@ const Header = (props) => {
     <header className='header'>
         <div className="header__inner container">
             <Logo className="header__logo" loading="eager" />
-            <nav className="header__menu">
+            <dialog className="header__overlay-menu-dialog">
+                <nav className="header__menu">
                 <ul className="header__menu-list">
                     {menuItems.map(({label, href}, index) => (
                         <li className='header__menu-item' key={index}>
@@ -42,25 +43,26 @@ const Header = (props) => {
                         </li>
                     ))}
                 </ul>
-            </nav>
-            <div className="header__actions">
-                    <Button 
-                    className="header__button" 
-                    label="Search"
-                    isLabelHidden
-                    mode="transparent"
-                    iconName="search"
-                    />
-                    <Button 
-                    className="header__button"
-                    label="Notification"
-                    isLabelHidden
-                    mode="transparent"
-                    iconName="notification"
-                    />
-            </div>
+                </nav>
+                <div className="header__actions">
+                        <Button 
+                        className="header__button" 
+                        label="Search"
+                        isLabelHidden
+                        mode="transparent"
+                        iconName="search"
+                        />
+                        <Button 
+                        className="header__button"
+                        label="Notification"
+                        isLabelHidden
+                        mode="transparent"
+                        iconName="notification"
+                        />
+                </div>
+            </dialog>
             <BurgerButton 
-            className="header__burger-button"
+            className="header__burger-button visible-tablet"
             />
         </div>
     </header>
