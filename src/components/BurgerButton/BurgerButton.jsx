b0df87e6f80993d11429c9ba9e5575a20301ebb2
Wergun1337 <derpywhoo1337@gmail.com>
2026-06-04 21:34:46 +0300

    burger button script added

--- src/components/BurgerButton/BurgerButton.jsx
+++ src/components/BurgerButton/BurgerButton.jsx
@@ -4,6 +4,7 @@ import './BurgerButton.scss'
 const BurgerButton = (props) => {
     const{
         className,
+        extraAttrs,
     }=props
 
     const title = 'Open menu'
@@ -12,7 +13,8 @@ const BurgerButton = (props) => {
             className={classNames(className, 'burger-button')}
             type="button"
             aria-label={title}
-            title={title}>
+            title={title}
+            {...extraAttrs}>
             <svg className="burger-button__svg" width="30" height="30" viewBox="0 0 100 100">
                 <path className="burger-button__line burger-button__line--1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"/>
                 <path className="burger-button__line burger-button__line--2" d="M 20,50 H 80"/>
